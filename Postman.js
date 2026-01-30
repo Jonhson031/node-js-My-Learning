78c34d2a816a244f619e7d6ba808b0e60b4f150e
Jonhson031 <jonhson228124@gmail.com>
2026-01-29 20:14:40 -0600

    more about http methods

---
+++ Postman.js
@@ -0,0 +1,42 @@
+/* 🧪 // ? What is Postman?
+
+Postman is a tool to test HTTP APIs without needing a frontend.
+
+It lets you:
+
+    - Send HTTP requests
+
+    - Inspect responses
+
+    - Test headers, body, status codes
+
+    - Debug backend logic fast
+
+
+? Think of it as a fake frontend for your backend.
+
+🧠 Why Postman is important
+
+    - When you build an API:
+
+    - There is no UI yet
+
+    - Browser can only easily do GET
+
+    - You need to test POST, PUT, DELETE, auth, headers, etc.
+
+👉 Postman solves that.
+
+? 📡 What can you send with Postman?
+
+    - HTTP methods: GET, POST, PUT, PATCH, DELETE
+
+    - Query strings
+
+    - Headers (Authorization, Content-Type)
+
+    - Request body (JSON, form-data)
+
+    - Cookies
+
+    - Auth tokens (JWT, Bearer)*/
