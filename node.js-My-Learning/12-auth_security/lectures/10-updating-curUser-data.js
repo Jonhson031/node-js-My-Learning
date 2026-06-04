56f0a69640fc2e940620eb70783300d746c23890
Jonhson031 <jonhson228124@gmail.com>
2026-06-03 22:40:45 -0500

    .

--- node.js-My-Learning/12-auth_security/lectures/10-updating-curUser-data.js
+++ node.js-My-Learning/12-auth_security/lectures/10-updating-curUser-data.js
@@ -1,5 +1,16 @@
 // * Updating current user data
 
+const filterObj = function (obj, ...allowedFields) {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  return newObj;
+};
+
 const updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
